ad30574ef3c6545716a646b1a0d0feb9785d43ff
Khaliga Ibrahimli <88455262+KhaligaIbrahimli@users.noreply.github.com>
2023-03-10 10:49:30 +0400

    First commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUsImage from "../elements/Aboutusimage";
+import Imagecolsection from "../elements/Imagecolsection";
+
+const About = () => {
+    return (
+        <div>
+            <AboutUsImage />
+            <Imagecolsection />
+        </div>
+    );
+};
+
+export default About;
